e4375d88e179c0e129dbcfe78317a3b4aa1a8f17
Daisuke Kogasaki <kgsk516@gmail.com>
2022-04-26 21:49:23 +0900

    スクリプトをクラス化したりして読みやすくした

--- get_steps.rb
+++ get_steps.rb
@@ -1,33 +1,63 @@
-require 'fitbit_api'
-require 'dotenv'
-require 'pry'
+# frozen_string_literal: true
 
-Dotenv.load
+require "fitbit_api"
+require "dotenv"
+require "pry"
 
-conn = Faraday.new(
-  url: 'https://api.fitbit.com/',
-  headers: {
-    'Content-Type' => 'application/x-www-form-urlencoded',
-    'Authorization' => "Basic #{ENV['AUTHORIZATION_BASIC']}",
-  }
-)
+class GetSteps
+  def initialize
+    Dotenv.load
+  end
 
-response = JSON.parse(conn.post('/oauth2/token', "grant_type=refresh_token&refresh_token=#{File.read('refresh')}").body)
+  def run
+    token = get_token
 
-File.open("refresh", mode = "w") { |f| f.write(response["refresh_token"]) }
+    # refreshファイルにrefresh_tokenを書き出す
+    File.open("refresh", "w") { |f| f.write(token["refresh_token"]) }
 
-client = FitbitAPI::Client.new(client_id: ENV['CLIENT_ID'],
-                               client_secret: ENV['CLIENT_SECRET'],
-                               access_token: response['access_token'],
-                               refresh_token: response['refresh_token'],
-                               user_id: response['user_id'])
+    steps = get_steps(token)
+    puts steps
 
-walked_steps = client.activity_time_series("steps", { period: "7d" })
+    post_pixela(steps)
+  end
 
-walked_steps.each do |step|
-  Faraday.post(
-    "https://pixe.la/v1/users/#{ENV['PIXELA_USER_ID']}/graphs/#{ENV['PIXELA_GLAPH_ID']}",
-    { 'date': step['dateTime'].gsub('-', ''), 'quantity': step['value'] }.to_json,
-    { "X-USER-TOKEN" => ENV['PIXELA_SECRET'] }
-  )
+  private
+
+  def faraday_client(target_url, options: {})
+    Faraday.new(
+      url: target_url,
+      headers: options,
+    )
+  end
+
+  def get_token
+    client = faraday_client("https://api.fitbit.com/",
+                            options: {
+                              "Content-Type" => "application/x-www-form-urlencoded",
+                              "Authorization" => "Basic #{ENV['AUTHORIZATION_BASIC']}"
+                            })
+    response = client.post("/oauth2/token", "grant_type=refresh_token&refresh_token=#{File.read('refresh')}").body
+    JSON.parse(response)
+  end
+
+  def get_steps(token)
+    client = FitbitAPI::Client.new(client_id: ENV["CLIENT_ID"],
+                                    client_secret: ENV["CLIENT_SECRET"],
+                                    access_token: token["access_token"],
+                                    refresh_token: token["refresh_token"],
+                                    user_id: token["user_id"])
+    client.activity_time_series("steps", { period: "7d" })
+  end
+
+  def post_pixela(walked_steps)
+    client = faraday_client("https://pixe.la/", options: { "X-USER-TOKEN" => ENV["PIXELA_SECRET"] })
+    walked_steps.each do |step|
+      client.post("v1/users/#{ENV['PIXELA_USER_ID']}/graphs/#{ENV['PIXELA_GLAPH_ID']}") do |req|
+        req.body = { 'date': step["dateTime"].delete("-"), 'quantity': step["value"] }.to_json
+      end
+    end
+  end
 end
+
+get_steps = GetSteps.new
+get_steps.run
